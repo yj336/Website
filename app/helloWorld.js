841101a289222ebb5f469138d303f253af67bde4
Diandian <diandian@DiandiandeMacBook.local>
2018-01-25 22:12:59 -0800

    Rounting

--- app/helloWorld.js
+++ app/helloWorld.js
@@ -1,34 +1,35 @@
 var http = require('http');
-      fs = require('fs');
-function serveStaticFile(res, path, Content-Type, responseCode){
+var fs = require('fs');
+console.log(__dirname)
+function serveStaticFile(res, path, contentType, responseCode){
 	if(!responseCode) responseCode = 200;
-	fs.readFile(__dirname + path, functino(err,data) {
+	fs.readFile(__dirname + path, function(err, data) {
 		if(err){
 			res.writeHead(500, { 'Content-Type': 'text/plain' });
 			res.end('500 - Internal Error');
 		} else{
 			res.writeHead(responseCode,
-				{'Content-Type': Content-Type });
+				{'Content-Type': contentType });
 			res.end(data);
 		}
 	});
 }
 
 
-http.createServer(function(req,res){
+http.createServer(function(req, res){
 	var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
 	switch(path){
 		case '':
-		    serveStaticFile(res, '/public/home.html)', 'text/html');
+		    serveStaticFile(res, '/public/home.html', 'text/html');
 		    break;
 		case '/about':
-		    serveStaticFile(res, '/public/about.html)', 'text/html');
+		    serveStaticFile(res, '/public/about.html', '');
 		    break;
-		case '/img/logo.jpg':
-		    serveStaticFile(res, '/public/img/logo.jpg)', 'image/jpeg');
+		case '/img/supportcat.jpg':
+		    serveStaticFile(res, '/public/img/supportcat.jpg', 'image/jpeg');
 		    break;
 		 default:
-		    serveStaticFile(res, '/public/404.html)', 'text/html', 404);
+		    serveStaticFile(res, '/public/404.html', 'text/html', 404);
 		    break;
 
 	}
